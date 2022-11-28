a3b390094707ae87aef61bb6f3894980eb7b6e6f
Fadro04 <fabian732@schule.bremen.de>
2022-11-28 15:12:22 +0100

    f

---
+++ src/main/java/de/fadro04/claimplugin/data/query/QueryProvider.java
@@ -0,0 +1,15 @@
+package de.fadro04.claimplugin.data.query;
+
+
+import net.cirellium.custommusic.data.sql.MySQL;
+import net.cirellium.custommusic.data.sql.SQLHelper;
+
+import net.cirellium.claimplugin.data.sql.MySQL;
+
+
+public class QueryProvider {
+
+    private SQLHelper helper;
+    private MySQL sql;
+
+}
